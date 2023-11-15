4173b3a09ec26bbfa4130b8e5d27a561672f096b
Michele Locati <michele@locati.it>
2023-11-15 10:21:18 +0100

    Test multiple files

--- src/test/java/VerifyTestCase.java
+++ src/test/java/VerifyTestCase.java
@@ -1,12 +1,13 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 import org.concretecms_community.concrete_cif.App;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -22,6 +23,24 @@ public class VerifyTestCase {
 	@MethodSource("badCifsProvider")
 	void testBadCifs(File file) {
 		assertEquals(1, App.execute(new String[] { file.getAbsolutePath() }));
+	}
+
+	@Test
+	void testMultipleCifs() {
+		File[] goodFiles = goodCifsProvider().limit(2).toArray(File[]::new);
+		assertNotSame(0, goodFiles.length);
+		File[] badFiles = (File[]) badCifsProvider().limit(2).toArray(File[]::new);
+		assertNotSame(0, badFiles.length);
+		assertEquals(1, App.execute(new String[] { badFiles[0].getAbsolutePath(), goodFiles[0].getAbsolutePath() }));
+		assertEquals(1, App.execute(new String[] { goodFiles[0].getAbsolutePath(), badFiles[0].getAbsolutePath() }));
+		if (goodFiles.length >= 2) {
+			assertEquals(0, App.execute(new String[] { goodFiles[0].getAbsolutePath(), goodFiles[1].getAbsolutePath() }));
+			assertEquals(1, App.execute(new String[] { goodFiles[0].getAbsolutePath(), badFiles[0].getAbsolutePath(), goodFiles[1].getAbsolutePath() }));
+		}
+		if (badFiles.length >= 2) {
+			assertEquals(1, App.execute(new String[] { badFiles[0].getAbsolutePath(), badFiles[1].getAbsolutePath() }));
+			assertEquals(1, App.execute(new String[] { badFiles[0].getAbsolutePath(), goodFiles[0].getAbsolutePath(), badFiles[1].getAbsolutePath() }));
+		}
 	}
 
 	static Stream<File> goodCifsProvider() {
